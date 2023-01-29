01d2305d439f931c0ce93f2a9d44fcccf29fe0f7
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 01:54:55 +0800

    vim-sleuth

---
+++ lua/plugins/vim-sleuth.lua
@@ -0,0 +1,14 @@
+-- smart detect document indentation
+-- https://github.com/tpope/vim-sleuth
+return {
+    'tpope/vim-sleuth',
+    config = function()
+        -- default neovim tabsize to 4 space
+        vim.opt.tabstop = 4
+        -- shift width match with tabstop
+        vim.opt.shiftwidth = 4
+        -- use space instead of tab char
+        vim.opt.expandtab = true
+    end
+}
+
