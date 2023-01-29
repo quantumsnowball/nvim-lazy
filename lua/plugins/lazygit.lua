dbf9187a1ea4fedb728c72191fe25f0d07a71fc9
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 01:11:24 +0800

    lazygit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,14 @@
+-- lazygit version control
+-- https://github.com/kdheepak/lazygit.nvim
+return {
+    'kdheepak/lazygit.nvim',
+    config = function()
+        -- config
+        -- vim.g.lazygit_floating_window_winblend = 30
+        vim.g.lazygit_floating_window_scaling_factor = 1.0
+        -- toggle keymap
+        local map = require('utils').map
+        map('n', '<leader>gg', ':LazyGit<CR>')
+    end
+}
+
