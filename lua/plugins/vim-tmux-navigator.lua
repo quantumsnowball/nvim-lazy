cdde6ed0db535b967f171ee346d2d3deb2c5640a
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 00:46:19 +0800

    vim-tmux-navigator

---
+++ lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,16 @@
+-- hjkl navigator in vim and tmux windows
+-- https://github.com/christoomey/vim-tmux-navigator
+return {
+    'christoomey/vim-tmux-navigator',
+    config = function()
+        local map = require('utils').map
+        local opts = { silent = true }
+        -- enable ctrl + hjkl only in normal mode
+        vim.g.tmux_navigator_no_mappings = 1
+        map('n', '<C-k>', ':TmuxNavigateUp<cr>', opts)
+        map('n', '<C-j>', ':TmuxNavigateDown<cr>', opts)
+        map('n', '<C-h>', ':TmuxNavigateLeft<cr>', opts)
+        map('n', '<C-l>', ':TmuxNavigateRight<cr>', opts)
+    end
+}
+
