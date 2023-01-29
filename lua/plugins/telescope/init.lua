4fdb4fc61345b692710c9667e7c24f933454b274
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 03:35:15 +0800

    restore telescope extensions configs

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -5,17 +5,10 @@ return {
     dependencies = 'nvim-lua/plenary.nvim',
     config = function()
         -- configs
-        --local core_cf = require('plugins.navigation.telescope.configs')
         local configs = require('plugins.telescope.configs')
-        --local ext_cf = require('plugins.navigation.telescope-nvim.extensions.configs')
-        --local configs = vim.tbl_extend('error', core_cf, ext_cf)
-        --local configs = vim.tbl_extend('error', core_cf )
-        -- print(vim.inspect(configs))
         require('telescope').setup(configs)
         -- telescope keymaps
         require('plugins.telescope.keymaps')
-        -- extensions
-	--require('plugins.navigation.telescope.extensions')
     end
 }
 
