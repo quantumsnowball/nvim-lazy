48c9ab6a0ac337803bc78ca9163f00a74dd33316
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 00:29:38 +0800

    nvim-tree

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,42 @@
+-- nvim-tree
+-- https://github.com/kyazdani42/nvim-tree.lua
+return {
+    'kyazdani42/nvim-tree.lua', 
+    tag = 'nightly',
+    dependencies = 'kyazdani42/nvim-web-devicons',
+    config = function()
+        require('nvim-tree').setup {
+            actions = {
+                change_dir = {
+                    enable = false,
+                },
+            },
+            reload_on_bufenter = true,
+            update_focused_file = {
+                enable = true,
+            },
+            view = {
+                float = {
+                    enable = true,
+                    open_win_config = {
+                        relative = 'win',
+                        height = 1000,
+                    }
+                },
+                adaptive_size = true,
+                mappings = {
+                    list = {
+                        { key = "<esc>", action = "close" }
+                    }
+                }
+            },
+            renderer = { highlight_opened_files = 'all' },
+            -- default hide dotfiles and ignored files
+            -- simply type H I to toggle show
+            filters = { dotfiles = true },
+            git = { ignore = true }
+        }
+        -- toggle keymap
+        require('utils').map('n', '<leader>e', ':NvimTreeToggle .<CR>', { silent = true })
+    end
+}
