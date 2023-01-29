e454fce17c851ec25428bce3180da84b2f0f38cd
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 01:44:50 +0800

    nvim-treesitter

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,21 @@
+-- syntax highlighting
+-- https://github.com/nvim-treesitter/nvim-treesitter
+return {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = function()
+        require('nvim-treesitter.configs').setup {
+            ensure_installed = 'all',
+            ignore_install = { "phpdoc" }, -- can never download successfully on macos
+            highlight = { enable = true },
+            incremental_selection = { enable = true },
+            textobjects = { enable = true },
+            indent = { enable = false }
+        }
+        -- enable code folding
+        vim.opt.foldmethod = 'expr'
+        vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+        vim.opt.foldlevel = 10
+    end
+}
+
