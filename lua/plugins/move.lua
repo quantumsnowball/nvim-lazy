b849dd2f992633730c00f0a70ce1bdf91be5f4de
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 03:10:03 +0800

    move

---
+++ lua/plugins/move.lua
@@ -0,0 +1,25 @@
+return {
+    'fedepujol/move.nvim',
+    config = function()
+        local map = require('utils').map
+        local opts = { silent = true }
+        -- Normal-mode
+        map('n', '<C-down>', ':MoveLine(1)<CR>', opts)
+        map('n', '<C-up>', ':MoveLine(-1)<CR>', opts)
+        -- map('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
+        -- map('n', '<A-l>', ':MoveHChar(1)<CR>', opts)
+
+        -- Visual-mode
+        --   ctrl + arrows
+        map('v', '<C-down>', ':MoveBlock(1)<CR>', opts)
+        map('v', '<C-up>', ':MoveBlock(-1)<CR>', opts)
+        map('v', '<C-left>', ':MoveHBlock(-1)<CR>', opts)
+        map('v', '<C-right>', ':MoveHBlock(1)<CR>', opts)
+        --   ctrl + hjkl
+        map('v', '<C-j>', ':MoveBlock(1)<CR>', opts)
+        map('v', '<C-k>', ':MoveBlock(-1)<CR>', opts)
+        map('v', '<C-h>', ':MoveHBlock(-1)<CR>', opts)
+        map('v', '<C-l>', ':MoveHBlock(1)<CR>', opts)
+    end
+}
+
