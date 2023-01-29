2e9be2be18c03b6ab21bd3682d8235c5fb77ad52
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 03:35:01 +0800

    telescope-ui-select

---
+++ lua/plugins/telescope-ui-select-nvim.lua
@@ -0,0 +1,12 @@
+-- telescope-ui-select
+-- https://github.com/nvim-telescope/telescope-ui-select.nvim
+-- TODO
+return {
+    'nvim-telescope/telescope-ui-select.nvim',
+    dependencies = 'nvim-telescope/telescope.nvim',
+    config = function()
+        local tele = require('telescope')
+        tele.load_extension("ui-select")
+    end
+}
+
