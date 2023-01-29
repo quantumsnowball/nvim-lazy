07c475aa16dc781fcd9b990701d7e58da8a8f900
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 02:41:52 +0800

    nvim-surround

---
+++ lua/plugins/nvim-surround.lua
@@ -0,0 +1,11 @@
+-- easy shortcuts to change surrounds
+-- https://github.com/kylechui/nvim-surround
+return {
+    'kylechui/nvim-surround',
+    config = function()
+        require('nvim-surround').setup {
+            -- Configuration here, or leave empty to use defaults
+        }
+    end
+}
+
