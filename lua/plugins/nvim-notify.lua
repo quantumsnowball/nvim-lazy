221e71c533ecc14613028529c8c4b86ea0abecf5
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 17:46:24 +0800

    nvim-notify

---
+++ lua/plugins/nvim-notify.lua
@@ -0,0 +1,21 @@
+-- Better `vim.notify()`
+-- https://github.com/rcarriga/nvim-notify
+return {
+    "rcarriga/nvim-notify",
+    keys = {
+        { "<leader>z", function()
+            require("notify").dismiss({ silent = true, pending = true })
+        end, desc = "Delete all Notifications", },
+    },
+    opts = {
+        timeout = 3000,
+        max_height = function()
+            return math.floor(vim.o.lines * 0.75)
+        end,
+        max_width = function()
+            return math.floor(vim.o.columns * 0.75)
+        end,
+        background_colour = "#ffffff",
+    },
+}
+
