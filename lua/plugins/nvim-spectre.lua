0341e7ac5c04c25ab3b2f52791b146b0a05b981c
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 16:17:05 +0800

    nvim-spectre

---
+++ lua/plugins/nvim-spectre.lua
@@ -0,0 +1,12 @@
+-- search/replace in multiple files
+-- https://github.com/nvim-pack/nvim-spectre
+return {
+    "windwp/nvim-spectre",
+    -- stylua: ignore
+    keys = {
+        { "<leader>sr", function() 
+            require("spectre").open() 
+        end, desc = "Replace in files (Spectre)" },
+    },
+}
+
