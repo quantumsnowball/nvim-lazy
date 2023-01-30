eb215c95d8a7a7961b3c09abd8a937ac424031e2
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 20:55:25 +0800

    nvim-cmp keymaps

--- lua/plugins/nvim-cmp/init.lua
+++ lua/plugins/nvim-cmp/init.lua
@@ -8,18 +8,26 @@ return {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
-        "saadparwaiz1/cmp_luasnip",
     },
     opts = function()
         local cmp = require("cmp")
         return {
+            completion = {
+                completeopt = "menu,menuone,noinsert",
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                -- ["<S-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
                 { name = "luasnip" },
                 { name = "buffer" },
                 { name = "path" },
             }),
-               
             formatting = {
                 format = function(_, item)
                     local icons = require("plugins.nvim-cmp/constants").icons.kinds
