1769b9f573d2e9342773caca191ccb81a263b5f6
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 18:28:20 +0800

    rft: utils.map default silent

--- lua/utils.lua
+++ lua/utils.lua
@@ -3,11 +3,12 @@ local M = {}
 -- create keymap with custom which-key hints
 function M.map(mode, keys, command, options, hint)
     -- set keymaps
+    options = vim.tbl_extend('force', { silent = true, desc = hint }, options or {} )
     vim.keymap.set(mode, keys, command, options)
     -- set hint
-    if hint then
-        require('which-key').register({ [keys] = hint })
-    end
+    -- if hint then
+    --     require('which-key').register({ [keys] = hint })
+    -- end
 end
 
 return M
