3f973482983e2d0d4e376b049531f215ba065e26
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 20:29:17 +0800

    displayed nvim-cmp icons for many kinds

--- lua/plugins/nvim-cmp/init.lua
+++ lua/plugins/nvim-cmp/init.lua
@@ -7,17 +7,28 @@ return {
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "saadparwaiz1/cmp_luasnip",
     },
     opts = function()
         local cmp = require("cmp")
         return {
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
-                -- { name = "luasnip" },
+                { name = "luasnip" },
                 { name = "buffer" },
                 { name = "path" },
             }),
-
+               
+            formatting = {
+                format = function(_, item)
+                    local icons = require("plugins.nvim-cmp/constants").icons.kinds
+                    if icons[item.kind] then
+                        item.kind = icons[item.kind] .. item.kind
+                    end
+                    return item
+                end,
+            },
         }
     end
 }
