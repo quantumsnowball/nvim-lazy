7f79ce99a5e147fe94fa31594683b49d25b47e09
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 12:50:19 +0800

    lazyload: telescope-tabman

--- lua/plugins/telescope-tabman.lua
+++ lua/plugins/telescope-tabman.lua
@@ -4,6 +4,14 @@ return {
     'quantumsnowball/telescope-tabman.nvim',
     branch = 'dev',
     dependencies = 'nvim-telescope/telescope.nvim',
+    keys = {
+        { '<leader>t', function()
+            tabman({ layout_config = layout_config })
+        end, 'telescope.tabpages()'},
+        { '<leader>ft', function()
+            tabman({ layout_config = layout_config, initial_mode = 'insert' })
+        end, 'telescope.tabpages()'},
+    },
     config = function()
         local tele = require('telescope')
         local tabman = tele.extensions.tabman.tabman
@@ -11,13 +19,6 @@ return {
         tele.load_extension('tabman')
         -- configs
         local layout_config = { width = 0.7, preview_height = 0.5, }
-        -- keymaps
-        require('utils').map('n', '<leader>t', function()
-            tabman({ layout_config = layout_config })
-        end, {}, 'telescope.tabpages()')
-        require('utils').map('n', '<leader>ft', function()
-            tabman({ layout_config = layout_config, initial_mode = 'insert' })
-        end, {}, 'telescope.tabpages()')
     end
 }
 
