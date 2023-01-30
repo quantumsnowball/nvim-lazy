bf371fa2d143f97f82f6aa4e88509e5480ac3bac
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-30 12:31:04 +0800

    lazyload: lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -2,14 +2,14 @@
 -- https://github.com/kdheepak/lazygit.nvim
 return {
     'kdheepak/lazygit.nvim',
+    keys = {
+        { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+        { '<leader>c', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
     config = function()
         -- config
         -- vim.g.lazygit_floating_window_winblend = 30
         vim.g.lazygit_floating_window_scaling_factor = 1.0
-        -- toggle keymap
-        local map = require('utils').map
-        map('n', '<leader>gg', ':LazyGit<CR>')
-        map('n', '<leader>c', ':LazyGit<CR>')
     end
 }
 
