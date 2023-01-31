0fd0d7ac15400c444c542ece864478380e8608e3
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-31 22:47:13 +0800

    ,r for lsp.rename

--- lua/plugins/nvim-lspconfig/keymaps.lua
+++ lua/plugins/nvim-lspconfig/keymaps.lua
@@ -1,7 +1,7 @@
 -- lsp  related keymaps
 return {
     -- editing
-    { '<leader>rn', vim.lsp.buf.rename, desc = 'lsp.buf.rename()' },
+    { '<leader>r', vim.lsp.buf.rename, desc = 'lsp.buf.rename()' },
     { '<leader>m', vim.lsp.buf.format, desc = 'lsp.buf.format()' },
     { '<leader>qf', vim.lsp.buf.code_action, desc = 'lsp.buf.code_action()' },
     -- code inspect
