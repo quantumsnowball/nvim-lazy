2d2f04f6735aee2739fe98d3d777531a5a01d2fd
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-31 18:18:01 +0800

    add theme variant toggle helper

--- lua/plugins/four-seasons.lua
+++ lua/plugins/four-seasons.lua
@@ -1,8 +1,30 @@
 return {
     'quantumsnowball/four-seasons.nvim',
     branch = 'dev',
+    event = 'VimEnter',
     config = function()
+        -- load
         require('four-seasons').setup({ palette = require('four-seasons').spring })
-        vim.cmd('colorscheme four-seasons-spring')
+        -- theme variants
+        local themes = {
+            'four-seasons-spring',
+            'four-seasons-summer',
+            'four-seasons-autumn',
+            'four-seasons-winter',
+        }
+        -- default theme
+        local id = 4
+        vim.cmd('colorscheme ' .. themes[id])
+        -- toggle keymap
+        require('utils').map('n', '<F3>', function()
+            local selected = themes[id]
+            print(selected)
+            vim.cmd('colorscheme ' .. selected)
+            if id >= 4 then
+                id = 1
+            else
+                id = id + 1
+            end
+        end)
     end,
 }
