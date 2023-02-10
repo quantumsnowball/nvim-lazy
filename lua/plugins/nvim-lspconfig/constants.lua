d4f1ec2b0b276d9ff35490db7f52cd5989faf31c
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-10 19:36:13 +0800

    don't install any lsp

--- lua/plugins/nvim-lspconfig/constants.lua
+++ lua/plugins/nvim-lspconfig/constants.lua
@@ -1,12 +1,12 @@
 local M = {}
 
 M.ensure_installed = {
-    "lua_ls", -- "sumneko_lua",
-    "pyright",
-    "ts_ls", -- "tsserver",
-    "cssls",
-    "jsonls",
-    "html",
+    -- "lua_ls", -- "sumneko_lua",
+    -- "pyright",
+    -- "ts_ls", -- "tsserver",
+    -- "cssls",
+    -- "jsonls",
+    -- "html",
 }
 
 return M
