8f3762ef0cd6608ca445e2613517ecd50e2710b3
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-10 19:45:42 +0800

    use https url for my own development repos

--- lua/plugins/four-seasons.lua
+++ lua/plugins/four-seasons.lua
@@ -1,7 +1,7 @@
 return {
     'quantumsnowball/four-seasons.nvim',
     branch = 'dev',
-    url = 'git@github.com:quantumsnowball/four-seasons.nvim.git',
+    url = 'https://github.com/quantumsnowball/four-seasons.nvim.git',
     event = 'VimEnter',
     config = function()
         -- load
