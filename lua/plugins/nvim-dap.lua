2370b38bc28a292399cf6a22ceb776bf21deba5e
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-14 18:58:24 +0800

    nvim-dap-ui working

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -5,25 +5,38 @@ return {
         'mfussenegger/nvim-dap',
         event = 'VeryLazy',
         keys = {
-            { ';b', function() require('dap').toggle_breakpoint() end }
+            { ';b',        function() require('dap').toggle_breakpoint() end },
+            { '<F9>',      function() require('dap').continue() end },
+            { '<space>ds', function() require('dap').continue() end },
+            { '<F10>',     function() require('dap').terminate() end },
+            { '<space>de', function() require('dap').terminate() end },
         }
     },
     {
         'rcarriga/nvim-dap-ui',
         dependencies = 'mfussenegger/nvim-dap',
         event = 'VeryLazy',
-        opts = {},
         keys = {
             { '<F7>', function() require('dapui').toggle() end },
-        }
+        },
+        config = function()
+            local dapui = require("dapui")
+            dapui.setup()
+            local dap = require("dap")
+            dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+            dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+            dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+        end
     },
     {
         'mfussenegger/nvim-dap-python',
         dependencies = 'mfussenegger/nvim-dap',
         event = 'VeryLazy',
-        opts = {},
+        keys = {
+            { '<F7>', function() require('dapui').toggle() end },
+        },
         config = function()
-            require('dap-python').setup('~/.pyenv/shims/python')
+            require('dap-python').setup('python')
             require('dap-python').test_runner = 'pytest'
         end
     }
