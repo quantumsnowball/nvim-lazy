0da8f316ebd3cd35561bb06646b16067f36c8856
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-15 11:33:55 +0800

    better search-replace keymaps

--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -4,11 +4,30 @@ return {
     "windwp/nvim-spectre",
     -- stylua: ignore
     keys = {
-        { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
-        { "<leader>sv", function() require("spectre").open_visual() end, mode = 'v',
-            desc = "Replace visual selected words (Spectre)" },
+        { "<leader>sr", function()
+            require("spectre").setup { open_cmd = 'new' }
+            require("spectre").open()
+        end, desc = "Replace in files (Spectre)" },
+        { "<leader>SR", function()
+            require("spectre").setup { open_cmd = 'vnew' }
+            require("spectre").open()
+        end, desc = "Replace in files (Spectre)" },
+        { "<leader>sr", function()
+            require("spectre").setup { open_cmd = 'new' }
+            require("spectre").open_visual({ select_word = true })
+        end, desc = "Replace visual selected words (Spectre)", mode = 'v' },
+        { "<leader>SR", function()
+            require("spectre").setup { open_cmd = 'vnew' }
+            require("spectre").open_visual({ select_word = true })
+        end, desc = "Replace visual selected words (Spectre)", mode = 'v' },
     },
     opts = {
-        open_cmd = 'tabnew'
+        mapping = {
+            ['run_current_replace'] = {
+                map = "qr",
+                cmd = "<cmd>lua require('spectre.actions').run_current_replace()<CR>",
+                desc = "replace current item"
+            }
+        }
     }
 }
