d36ec5bf277b740c1e7d343e9585be838f67b886
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-15 17:26:00 +0800

    only a few useful tree-sitter parsers

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,3 +1,39 @@
+local selected_parser = {
+    -- neovim
+    'lua',
+    'vim',
+    -- general
+    'make',
+    'bash',
+    'markdown',
+    'markdown_inline',
+    'latex',
+    'sql',
+    'ini',
+    'toml',
+    'yaml',
+    'regex',
+    -- git
+    'gitignore',
+    'git_rebase',
+    'gitcommit',
+    'gitattributes',
+    -- python
+    'python',
+    -- webdev
+    'tsx',
+    'typescript',
+    'javascript',
+    'css',
+    'scss',
+    'html',
+    'json',
+    'json5',
+    'vue',
+    'http',
+    -- web3
+    'solidity',
+}
 -- syntax highlighting
 -- https://github.com/nvim-treesitter/nvim-treesitter
 return {
@@ -6,7 +42,7 @@ return {
     event = 'BufReadPost',
     config = function()
         require('nvim-treesitter.configs').setup {
-            ensure_installed = 'all',
+            ensure_installed = selected_parser,
             ignore_install = { "phpdoc" }, -- can never download successfully on macos
             highlight = { enable = true },
             incremental_selection = { enable = true },
