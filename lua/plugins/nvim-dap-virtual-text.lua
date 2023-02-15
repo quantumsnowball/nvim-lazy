22602ef80f8f80e87080f40f1103cb14503d8222
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-15 12:21:32 +0800

    better dap virtual text

--- lua/plugins/nvim-dap-virtual-text.lua
+++ lua/plugins/nvim-dap-virtual-text.lua
@@ -2,5 +2,7 @@
 -- https://github.com/theHamsta/nvim-dap-virtual-text
 return {
     'theHamsta/nvim-dap-virtual-text',
-    opts = {}
+    opts = {
+        highlight_new_as_changed = true,
+    }
 }
