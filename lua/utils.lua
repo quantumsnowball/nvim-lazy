84aaf4e72b69d9c9fbeb6d0c055b457b71841296
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-16 10:45:14 +0800

    utils fn to get current fn name

--- lua/utils.lua
+++ lua/utils.lua
@@ -17,4 +17,30 @@ function M.debug(arg)
     print(res)
 end
 
+-- get nearest function name
+function M.get_current_function_name()
+    local ts_utils = require('nvim-treesitter.ts_utils')
+    local current_node = ts_utils.get_node_at_cursor()
+
+    if not current_node then
+        print('Current node not found.')
+        return ""
+    end
+
+    local expr = current_node
+    while expr do
+        if expr:type() == 'function_definition' then
+            break
+        end
+        expr = expr:parent()
+    end
+
+    if not expr then
+        print('Function definition not found')
+        return ""
+    end
+
+    return (ts_utils.get_node_text(expr:child(1)))[1]
+end
+
 return M
