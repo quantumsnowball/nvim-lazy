ce058564eb1e6dc1c5af0c283e2b38d6f034e120
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-21 10:08:51 +0800

    fix open_visual

--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -12,14 +12,14 @@ return {
             require("spectre").setup { open_cmd = 'vnew' }
             require("spectre").open()
         end, desc = "Replace in files (Spectre)" },
-        { "<leader>sr", function()
-            require("spectre").setup { open_cmd = 'new' }
-            require("spectre").open_visual()
-        end, desc = "Replace visual selected words (Spectre)", mode = 'v' },
-        { "<leader>SR", function()
-            require("spectre").setup { open_cmd = 'vnew' }
-            require("spectre").open_visual()
-        end, desc = "Replace visual selected words (Spectre)", mode = 'v' },
+        -- { "<leader>sr", function()
+        --     require("spectre").setup { open_cmd = 'new' }
+        --     require("spectre").open_visual()
+        -- end, desc = "Replace visual selected words (Spectre)", mode = 'v' },
+        -- { "<leader>SR", function()
+        --     require("spectre").setup { open_cmd = 'vnew' }
+        --     require("spectre").open_visual()
+        -- end, desc = "Replace visual selected words (Spectre)", mode = 'v' },
     },
     opts = {
         mapping = {
@@ -29,5 +29,14 @@ return {
                 desc = "replace current item"
             }
         }
-    }
+    },
+    init = function()
+        -- open_visual is delayed by one if setting up using keys props
+        -- temp fix: setup keymaps using vim cmd
+        --
+        -- step: open_visual, pull to far bottom
+        vim.cmd "vnoremap <leader>sr <esc>:lua require('spectre').open_visual()<CR><c-w>J"
+        -- step: open_visual, pull to far right
+        vim.cmd "vnoremap <leader>SR <esc>:lua require('spectre').open_visual()<CR><c-w>L"
+    end
 }
