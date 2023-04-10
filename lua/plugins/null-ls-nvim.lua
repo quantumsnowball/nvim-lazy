f370ac1327a761fc1c4a792a91182c49d80f1200
Quantum Snowball <quantum.snowball@gmail.com>
2023-04-10 22:35:04 +0800

    autopep8 default max-line-length 120

--- lua/plugins/null-ls-nvim.lua
+++ lua/plugins/null-ls-nvim.lua
@@ -6,7 +6,9 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.autopep8,
+				null_ls.builtins.formatting.autopep8.with({
+					extra_args = { '--max-line-length', '120' }
+				}),
 				-- null_ls.builtins.completion.spell,
 				-- null_ls.builtins.formatting.stylua,
 				-- null_ls.builtins.diagnostics.eslint,
