2746c937a73c2dea476a235d3f27558880c22736
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-28 21:31:28 +0800

    don't set colorscheme too soon

--- lua/plugins/four-seasons.lua
+++ lua/plugins/four-seasons.lua
@@ -2,7 +2,7 @@ return {
     'quantumsnowball/four-seasons.nvim',
     branch = 'dev',
     url = 'git@github.com:quantumsnowball/four-seasons.nvim.git',
-    event = 'VimEnter',
+    -- event = 'VimEnter',
     config = function()
         -- load
         require('four-seasons').setup({ palette = require('four-seasons').spring })
