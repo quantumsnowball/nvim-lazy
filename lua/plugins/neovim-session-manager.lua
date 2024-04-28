b7e0789bb2951dae21303bdb8c94e527cc22a4e3
Quantum Snowball <quantum.snowball@gmail.com>
2024-04-28 23:51:37 +0800

    autoload Alpha screen when there is only a [No Name] empty buffer exists

--- lua/plugins/neovim-session-manager.lua
+++ lua/plugins/neovim-session-manager.lua
@@ -10,15 +10,23 @@ return {
             -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
             autosave_ignore_not_normal = false,
         })
-        -- autoload file explorer upon session loaded
-        local config_group = vim.api.nvim_create_augroup('SessionManagerCommandGroup', {})
-        -- A global group for all your config autocommands
-        -- vim.api.nvim_create_autocmd({ 'SessionLoadPost' }, {
-        --     group = config_group,
-        --     callback = function()
-        --         -- auto start nvim-tree when session loaded
-        --         require('nvim-tree').toggle(false, true)
-        --     end,
-        -- })
+
+        -- autoload Alpha screen when there is only a [No Name] empty buffer exists
+        local config_group = vim.api.nvim_create_augroup('MyConfigGroup', {}) -- A global group for all your config autocommands
+        vim.api.nvim_create_autocmd({ 'User' }, {
+            pattern = "SessionLoadPost",
+            group = config_group,
+            callback = function()
+                local bufs = vim.api.nvim_list_bufs()
+                local bufs_n = #bufs
+                local buf_id = bufs[1]
+                vim.print(buf_id)
+                local buffer_name = vim.api.nvim_buf_get_name(buf_id)
+                if bufs_n == 1 and buffer_name == '' then
+                    vim.cmd('Alpha')
+                    vim.api.nvim_buf_delete(buf_id, {})
+                end
+            end,
+        })
     end
 }
