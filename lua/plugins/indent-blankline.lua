98ece383362c561afcff2c34de1d52d9705e6eb8
Quantum Snowball <quantum.snowball@gmail.com>
2024-04-28 19:34:40 +0800

    fix wrong shiftwidth

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,7 +2,7 @@
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 return {
     'lukas-reineke/indent-blankline.nvim',
-    event = 'VeryLazy',
+    event = 'BufReadPre',
     main = 'ibl',
     config = function()
         -- show space and end-of-line
