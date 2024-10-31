228d4d7b1ef0752d87e4a1090330a58fdc0f8eeb
Quantum Snowball <quantum.snowball@gmail.com>
2024-10-31 15:36:01 +0800

    use lua impl

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -1,16 +1,22 @@
--- hjkl navigator in vim and tmux windows
--- https://github.com/christoomey/vim-tmux-navigator
+-- hjkl navigator in neovim and tmux windows
+-- vim https://github.com/christoomey/vim-tmux-navigator
+-- lua https://github.com/alexghergh/nvim-tmux-navigation
+-- TODO: still not able to nav away from neovim
 return {
-    'christoomey/vim-tmux-navigator',
+    'alexghergh/nvim-tmux-navigation',
     event = 'VeryLazy',
-    keys = {
-        { '<C-k>', ':TmuxNavigateUp<cr>',    { silent = true } },
-        { '<C-j>', ':TmuxNavigateDown<cr>',  { silent = true } },
-        { '<C-h>', ':TmuxNavigateLeft<cr>',  { silent = true } },
-        { '<C-l>', ':TmuxNavigateRight<cr>', { silent = true } },
-    },
     config = function()
-        -- enable ctrl + hjkl only in normal mode
-        vim.g.tmux_navigator_no_mappings = 1
+        local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+        nvim_tmux_nav.setup {
+            disable_when_zoomed = true -- defaults to false
+        }
+
+        vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+        vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+        vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+        vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+        -- vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+        -- vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
     end
 }
