7a80d87cfa259a759295d5181fbd7fbdaa5b7569
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-02 00:13:23 +0800

    docs

--- lua/plugins/barbecue-nvim.lua
+++ lua/plugins/barbecue-nvim.lua
@@ -1,6 +1,7 @@
+-- This is a VS Code like winbar that uses nvim-navic in order to get LSP context from your language server.
+-- https://github.com/utilyre/barbecue.nvim
 return {
     "utilyre/barbecue.nvim",
-    -- enabled = false,
     name = "barbecue",
     version = "*",
     dependencies = {
