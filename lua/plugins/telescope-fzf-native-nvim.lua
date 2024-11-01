29f1189eaccb0f293f39b6865d044f8d1f56e20f
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-01 23:11:11 +0800

    wiki: Error: failed to run config for telescope-fzf-native.nvim

--- lua/plugins/telescope-fzf-native-nvim.lua
+++ lua/plugins/telescope-fzf-native-nvim.lua
@@ -1,5 +1,13 @@
 -- fzf telescope extension
 -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
+-- wiki:
+-- 1. Error: failed to run config for telescope-fzf-native.nvim
+--    This tool need to build using gcc, clang and make, make sure these are installed
+--    If these C tools are not installed during nvim first launch, chance are the build
+--    process will fail. Solution is to go to ~/.local/share/nvim/lazy/ and delete the
+--    telescope-fzf-native.nvim/ directory. Re launch nvim and start the installation
+--    all over again.
+--
 return {
     'nvim-telescope/telescope-fzf-native.nvim',
     event = 'VeryLazy',
